d532792ccb3edab386ed00349c4d4c2a1216c8df
yangunbrother-eng <yangunbrother-eng@users.noreply.github.com>
2026-09-22 13:48:11 +0900

    Expand Daangn gold business sources

--- lib/daangn-profiles.ts
+++ lib/daangn-profiles.ts
@@ -1,6 +1,10 @@
 export const GOLD_PROFILES = [
 "금니를부탁해-mse5bq4dybj4", "천안-금니-거래소-j558cq9hnsfs", "당진-현대금거래소-65982z833o3n",
 "한국금거래소안성공도점-o226v5t13i3c", "한국공인금거래소-부천소사지점-wtknsi4w31wv", "한국g1금거래소-2jvsbro8d71c",
+"한국금거래소-용산점-76ohgzfqp3ai", "한국금거래소송파잠실-jhnfrb445mpu", "한국금거래소-금천점-a5tcdqoukiu2", "한국금거래소-성동옥수점-ewexyg4qrrdf",
+"한국금거래소-대구달서점-wwrx4qfx89m1", "한국공인금거래소-인천검단지점-zb4bphhpx5t1", "한국mk금거래소-6s387j65xhhi",
+"한국금거래소-경산점-dvxgd4zx43sc", "대한제일금거래소-f4s11gggsot3", "금거래소-uwy1h8mgfe2w", "로얄금거래소-fktd87ok49vg",
+"순금사랑-금거래소-ekj49ups72ud", "더드림-한국금거래소-p6kxbyzepdub", "금거래소-노블레스-ouaj6ijk41iy", "한국금거래소가좌dmc점-dgcm72n4d1hu", "금이당-금거래소-주얼리-cecdc8ec6m1z",
 "대성금거래소-ppe1jg3ih4gk", "금센티브-jwc7j2ocam1e", "금빛세상-금거래소-czfi6a889je9", "골드스톤-금거래소-s617r8jq1zj3",
 "금사랑금거래소-수완점-7imn4q5khoeg", "퍼스트금거래소-m7vcgew2w1ac", "마산금거래소-쥬얼리-tv41ba84fyuq", "종로직영-금거래소-위례본점-sgfpebh7b1ow", "사계절금거래소서구가정점-sjan8p8i8z77",
 "금니톡톡-9b4baw1jsevn", "금니2849-wg4gd6z4qkvq", "금니판매-거래소-ioj28211df3j", "금니마켓-orsn7c7ja1x2", "바른-금니-안양점-ots2ymm1hfos", "금니거래소-gvsne96k6fz7", "대한공인금거래소강남점-wjmudyhwjfmi", "금이빨센터-송파점-c3pn9w4s1o5m", "태산금속-xk8abnzrnivg", "금박사-cnqyyakaugwq",
